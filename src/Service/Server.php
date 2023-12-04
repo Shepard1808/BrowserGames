6b492193df3df5ab721d1dda9a93990fbc1c36ad
Florian Bartz <fbartz@nb-fbartz.office.databay.de>
2023-12-04 13:11:47 +0100

    working skeleton with loop finished

--- src/Service/Server.php
+++ src/Service/Server.php
@@ -6,6 +6,7 @@ use Fbartz\BrowserGames\Controller\Http\HttpController;
 use Fbartz\BrowserGames\Controller\Websocket\WebsocketController;
 use React\EventLoop\Loop;
 use React\MySQL\Factory;
+use React\Socket\SocketServer;
 
 class Server
 {
@@ -23,6 +24,9 @@ class Server
 
         $this->websocketController = new WebsocketController();
         $this->httpController = new HttpController($loop,$this->websocketController->getMiddleware());
+
+        $this->httpController->getServer()->listen(new SocketServer("0.0.0.0:55498"));
+        $loop->run();
 
     }
 
--- src/index.php
+++ src/index.php
@@ -5,8 +5,11 @@ namespace BrowserGames;
 require '../vendor/autoload.php';
 
 use Fbartz\BrowserGames\Service\Database;
+use Fbartz\BrowserGames\Service\Server;
 
 $dbData = parse_ini_file("setup.ini",false,INI_SCANNER_RAW);
 
 $database = Database::getInstance();
 $database::initConnection($dbData);
+
+new Server();
